163db309c25b42cfba6a944354d896e52306724b
r3nanp <renanmol87@gmail.com>
2020-11-13 11:39:51 -0300

    docs: add eslint and prettier to format the code

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['standard', 'prettier/standard'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'space-before-function-paren': 'off'
+  },
+}
