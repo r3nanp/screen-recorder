9d8854af30d3b9e0bd07a5fb4fde82b683c0b00b
r3nanp <renanmol87@gmail.com>
2020-12-09 09:22:19 -0300

    feat: add webpack

---
+++ webpack/electron.webpack.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+const rootPath = path.resolve(__dirname, '..')
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+  mode: isDevelopment ? 'development' : 'production',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devtool: 'source-map',
+  entry: path.resolve(rootPath, 'electron', 'main.js'),
+  target: 'electron-main',
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(rootPath, './dist'),
+    filename: '[name].js',
+  },
+}
